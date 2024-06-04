6a00abe15710d00f3b535007dab7076577764c2d
mmccjjj <dhuber88@sunrise.ch>
2024-06-04 16:08:13 +0200

    temp

--- script.js
+++ script.js
@@ -1,41 +1,35 @@
 let timerInt;
-let sec= 0;
-let min= 0;
-let hour= 0;
-let day= 0;
+let sec;
+let min;
+let hour;
+let day;
 
 
-function timer() { 
-
-if (sec === 59){
-    sec= 0;
-
-    if (min === 59){
-        min= 0;
-
-        if (hour=== 23){
-           hour= 0;
-           day++; 
-           document.getElementById("timerDay").innerText= day;
-        }
-
-    else{
-    hour++;
+function timer(){
+    if(sec=== 0){
+        if(min=== 0){
+            if(hour=== 0){
+                if(day=== 0){
+                    stopTimer();
+                    return;
+                }
+                day--;
+                hour= 23;
+                min= 59;
+                sec= 59;
+               }
+                else{
+                    hour--;
+                    min=59
+                    sec=59
+                }
+               }else{
+                min--;
+                sec= 59
+               }
     }
-    document.getElementById("timerHour").innerText= hour;
-    }
-    else{
-    min++;
-    }
-    document.getElementById("timerMin").innerText= min;
-    }
-
-    else{
-    sec++;
-    }
-
-document.getElementById("timerSec").innerText= sec;
 }
+
 
 
 function startTimer(){

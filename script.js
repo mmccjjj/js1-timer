cf14788543a7b6244f6e7ecfcb126a8e483c3e93
mmccjjj <dhuber88@sunrise.ch>
2024-06-05 11:22:52 +0200

    logik funktioniert

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 let timerInt;
 let sec;
 let min;
@@ -6,15 +7,18 @@ let day;
 let alarmSound= new Audio("/sounds/alarm-clock-70648.mp3");
 
 
+
+
 function timer(){
-    if(sec=== 0){
-        if(min=== 0){
-            if(hour=== 0){
-                if(day=== 0){
+    if(sec== 0){
+        if(min== 0){
+            if(hour== 0){
+                if(day== 0){
                     stopTimer();
                     alarmSound.play();
                     alert("Timer ist fertig");
                     return;
+                    
                 }
                 day--;
                 hour= 23;
@@ -33,7 +37,12 @@ function timer(){
               }else{
                 sec--;
               }
-
+    
+    
+    hour= hour.toString().padStart(2,"0")
+    min= min.toString().padStart(2,"0")
+    sec= sec.toString().padStart(2,"0")
+       
     document.getElementById("timerDay").innerText= day
     document.getElementById("timerHour").innerText= hour
     document.getElementById("timerMin").innerText= min
@@ -44,11 +53,14 @@ function timer(){
 
 function startTimer(){
     if (!timerInt){
+        if (day== 0&& hour== 0&& min== 0&& sec== 0) {
+
         day = document.getElementById("inDay").value || 0;
         hour = document.getElementById("inHour").value || 0;
         min = document.getElementById("inMin").value || 0;
         sec = document.getElementById("inSec").value || 0;
-        timerInt= setInterval(timer,1000);
+        }
+        timerInt= setInterval(timer,10);
 }
 }
 
@@ -63,9 +75,18 @@ function resetTimer(){
     min= 0;
     hour= 0;
     day= 0;
+
+    hour= hour.toString().padStart(2,"0");
+    min= min.toString().padStart(2,"0");
+    sec= sec.toString().padStart(2,"0");
+
     document.getElementById("timerSec").innerText= sec;
     document.getElementById("timerMin").innerText= min;
     document.getElementById("timerHour").innerText= hour;
     document.getElementById("timerDay").innerText= day;
+    document.getElementById("inSec").value= null;
+    document.getElementById("inMin").value= null;
+    document.getElementById("inHour").value= null;
+    document.getElementById("inDay").value= null;
 }
 
